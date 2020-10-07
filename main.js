41901774df7cc98042ab3d6f7913b8734e0e3bf9
Chirodea Mihai <37183680+MChirodea@users.noreply.github.com>
2020-10-07 10:54:26 +0300

    Update L8

--- main.js
+++ main.js
@@ -1,8 +1,48 @@
 const app = Vue.createApp({
     data(){
         return{
-            product: 'Lamp',
-            description: 'Socks are good'
+            product: 'Socks',
+            description: 'Socks are good',
+            image: './assets/images/socks_green.jpg',
+            url:'http://www.google.ro',
+            inventory: 0,
+            onSale: false,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants:[
+                {id: 2334, color: 'green', image:'./assets/images/socks_green.jpg'},
+                {id:2235, color:'blue', image:'./assets/images/socks_blue.jpg'},
+            ],
+            inStock: false,
+            sizes:[
+                {id: 122, size:"XL"},
+                {id: 121, size:"XXL"},
+                {id: 132, size:"L"},
+            ],
+
+            cart: 0,
+
+            codingChallenges: false,
+            brand: 'Vue Mastery',
+        }
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1
+        },
+
+        decrementToCart(){
+            if(this.cart > 0){
+                this.cart -= 1
+            }  
+        },
+
+        updateImage(variantImage){
+            this.image = variantImage
+        },
+    },
+    computed:{
+        title(){
+            return this.brand + ' ' + this.product
         }
     }
 })
